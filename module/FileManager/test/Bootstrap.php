442c2c1ac3a63f998d75585f22b64480c2dabb0d
Alex Ermashev <alexermashev@gmail.com>
2015-01-12 13:51:30 +0600

    fix a small bug with public dir path in the file manager's tests

--- module/FileManager/test/Bootstrap.php
+++ module/FileManager/test/Bootstrap.php
@@ -2,7 +2,7 @@
 namespace FileManager\Test;
 
 define('APPLICATION_ROOT', '../../../');
-define('APPLICATION_PUBLIC', APPLICATION_ROOT . 'public');
+define('APPLICATION_PUBLIC', APPLICATION_ROOT . 'public_html');
 require_once APPLICATION_ROOT . 'init_tests_autoloader.php';
 
 use UnitTestBootstrap;
