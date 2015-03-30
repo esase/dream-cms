2bc2395f6e34cd455bf1cab65c127e6f6cd4ea3e
= <alexermashev@gmail.com>
2015-03-30 12:28:16 +0600

    issue #84

--- module/Page/src/Page/View/Helper/PageMenu.php
+++ module/Page/src/Page/View/Helper/PageMenu.php
@@ -13,6 +13,12 @@ class PageMenu extends AbstractHelper
     protected $pagesTree = [];
 
     /**
+     * Max level
+     * @var integer
+     */
+    protected $maxLevel = 0;
+
+    /**
      * Class constructor
      *
      * @param array $pagesTree
@@ -25,10 +31,13 @@ class PageMenu extends AbstractHelper
     /**
      * Page menu
      *
+     * @param integer $maxLevel
      * @return string
      */
-    public function __invoke()
+    public function __invoke($maxLevel = 0)
     {
+        $this->maxLevel = $maxLevel;
+
         return $this->getView()->partial('page/partial/menu', [
             'menu_items' => $this->processMenuItems($this->pagesTree)
         ]);
@@ -38,9 +47,10 @@ class PageMenu extends AbstractHelper
      * Process menu items
      *
      * @param array $pages
+     * @param integer $level
      * @return string
      */
-    protected function processMenuItems(array $pages)
+    protected function processMenuItems(array $pages, $level = 1)
     {
         $menu = null;
 
@@ -56,18 +66,26 @@ class PageMenu extends AbstractHelper
                         }
                     }
                     else {
-                        $menu .= $this->getView()->partial('page/partial/menu-item-start', [
-                            'url' => $pageUrl,
-                            'title' => $this->getView()->pageTitle($pageOptions)
-                        ]);
+                        $childrenMenu = null;
 
                         // check for children
-                        if (!empty($pageOptions['children'])) {
-                            if (null !== ($children = $this->processMenuItems($pageOptions['children']))) {
-                                $menu .= $this->getView()->partial('page/partial/menu-item-children', [
+                        if (!empty($pageOptions['children']) && (!$this->maxLevel || $level < $this->maxLevel)) {
+                            if (null !== ($children = $this->processMenuItems($pageOptions['children'], ($level + 1)))) {
+                                $childrenMenu = $this->getView()->partial('page/partial/menu-item-children', [
                                     'children' => $children,
                                 ]);
                             }
+                        }
+
+                        $menu .= $this->getView()->partial('page/partial/menu-item-start', [
+                            'url' => $pageUrl,
+                            'title' => $this->getView()->pageTitle($pageOptions),
+                            'children' => $childrenMenu !== null
+                        ]);
+
+
+                        if ($childrenMenu) {
+                            $menu .= $childrenMenu;
                         }
 
                         $menu .= $this->getView()->partial('page/partial/menu-item-end');
