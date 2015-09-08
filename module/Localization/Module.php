407ad14d79f4359d380ec3171070da745ab1330b
esase <alexermashev@gmail.com>
2015-09-08 17:22:16 +0600

    Add a license info in Localization module

--- module/Localization/Module.php
+++ module/Localization/Module.php
@@ -1,61 +1,88 @@
 <?php
+
+/**
+ * EXHIBIT A. Common Public Attribution License Version 1.0
+ * The contents of this file are subject to the Common Public Attribution License Version 1.0 (the “License”);
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.dream-cms.kg/en/license. The License is based on the Mozilla Public License Version 1.1
+ * but Sections 14 and 15 have been added to cover use of software over a computer network and provide for
+ * limited attribution for the Original Developer. In addition, Exhibit A has been modified to be consistent
+ * with Exhibit B. Software distributed under the License is distributed on an “AS IS” basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the specific language
+ * governing rights and limitations under the License. The Original Code is Dream CMS software.
+ * The Initial Developer of the Original Code is Dream CMS (http://www.dream-cms.kg).
+ * All portions of the code written by Dream CMS are Copyright (c) 2014. All Rights Reserved.
+ * EXHIBIT B. Attribution Information
+ * Attribution Copyright Notice: Copyright 2014 Dream CMS. All rights reserved.
+ * Attribution Phrase (not exceeding 10 words): Powered by Dream CMS software
+ * Attribution URL: http://www.dream-cms.kg/
+ * Graphic Image as provided in the Covered Code.
+ * Display of Attribution Information is required in Larger Works which are defined in the CPAL as a work
+ * which combines Covered Code or portions thereof with code not governed by the terms of the CPAL.
+ */
 namespace Localization;
 
 use Localization\Service\Localization as LocalizationService;
 use Acl\Model\AclBase as AclBaseModel;
 use Application\Utility\ApplicationErrorLogger;
 use Application\Service\ApplicationSetting as SettingService;
+use User\Service\UserIdentity as UserIdentityService;
 use Zend\ModuleManager\ModuleManagerInterface;
 use Zend\ModuleManager\ModuleEvent as ModuleEvent;
 use Zend\Console\Request as ConsoleRequest;
 use Zend\Validator\AbstractValidator;
 use Zend\Mvc\MvcEvent;
 use Zend\Http\Header\SetCookie;
-use User\Service\UserIdentity as UserIdentityService;
 use Exception;
 use Locale;
 
 class Module
 {
     /**
+     * Localization cookie
+     */
+    CONST LOCALIZATION_COOKIE = 'language';
+
+    /**
      * User identity
+     *
      * @var array
      */
     protected $userIdentity;
 
     /**
      * List of registered localizations
+     *
      * @var array
      */
     protected $localizations;
 
     /**
      * Default localization
+     *
      * @var array
      */
     protected $defaultLocalization;
 
     /**
      * Service locator
-     * @var object
+     *
+     * @var \Zend\ServiceManager\ServiceManager
      */
     protected $serviceLocator;
 
     /**
      * Module manager
-     * @var object
+     *
+     * @var \Zend\ModuleManager\ModuleManagerInterface
      */
     protected $moduleManager;
 
     /**
-     * Localization cookie
-     */ 
-    CONST LOCALIZATION_COOKIE = 'language';
-
-    /**
      * Init
      *
-     * @param object $moduleManager
+     * @param \Zend\ModuleManager\ModuleManagerInterface
+     * @return void
      */
     public function init(ModuleManagerInterface $moduleManager)
     {
@@ -66,11 +93,14 @@ class Module
         $this->moduleManager = $moduleManager;
 
         $moduleManager->getEventManager()->
-            attach(ModuleEvent::EVENT_LOAD_MODULES_POST, [$this, 'initApplication']);
+                attach(ModuleEvent::EVENT_LOAD_MODULES_POST, [$this, 'initApplication']);
     }
 
     /**
      * Bootstrap
+     *
+     * @param \Zend\Mvc\MvcEvent $e
+     * @return void
      */
     public function onBootstrap(MvcEvent $e)
     {
@@ -87,7 +117,8 @@ class Module
     /**
      * Init application
      * 
-     * @param object $e
+     * @param \Zend\ModuleManager\ModuleEvent $e
+     * @return void
      */
     public function initApplication(ModuleEvent $e)
     {
@@ -99,6 +130,8 @@ class Module
 
     /**
      * Init default localization
+     *
+     * @return void
      */
     private function initDefaultLocalization()
     {
@@ -147,7 +180,8 @@ class Module
     /**
      * Init user localization
      *
-     * @param object $e MvcEvent
+     * @param \Zend\Mvc\MvcEvent $e
+     * @return void
      */
     public function initUserLocalization(MvcEvent $e)
     {
@@ -163,13 +197,15 @@ class Module
                     // set default language
                     $router->setDefaultParam('language', $this->defaultLocalization['language']);
 
-                    // remember user's choosen language
+                    // remember user's chose language
                     $this->setUserLanguage($this->defaultLocalization['language']);
+
                     return;
                 }
 
                 // show a 404 page
-                return $matches->setParam('action', 'not-found');
+                $matches->setParam('action', 'not-found');
+                return;
             }
 
             // init an user localization
@@ -203,7 +239,7 @@ class Module
         if (!$this->userIdentity['language'] || $this->userIdentity['language'] != $language) {
             // save language
             if ($this->userIdentity['role'] != AclBaseModel::DEFAULT_ROLE_GUEST) {
-                $model = $this->serviceLocator
+                 $this->serviceLocator
                     ->get('Application\Model\ModelManager')
                     ->getInstance('User\Model\UserBase')
                     ->setUserLanguage($this->userIdentity['user_id'], $language);
@@ -234,19 +270,20 @@ class Module
     {
         return [
             'Zend\Loader\ClassMapAutoloader' => [
-                __DIR__ . '/autoload_classmap.php',
+                __DIR__ . '/autoload_classmap.php'
             ],
             'Zend\Loader\StandardAutoloader' => [
                 'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ],
-            ],
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__
+                ]
+            ]
         ];
     }
 
-
     /**
      * Get service config
+     *
+     * @return array
      */
     public function getServiceConfig()
     {
@@ -256,6 +293,8 @@ class Module
 
     /**
      * Init view helpers
+     *
+     * @return array
      */
     public function getViewHelperConfig()
     {
@@ -272,7 +311,7 @@ class Module
     /**
      * Return path to config file
      *
-     * @return boolean
+     * @return array
      */
     public function getConfig()
     {
