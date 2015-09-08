cdca7b80dafc80384099dc93118e72b804c06944
esase <alexermashev@gmail.com>
2015-09-08 15:19:18 +0600

    Add a license info in Install module

--- module/Install/Module.php
+++ module/Install/Module.php
@@ -1,8 +1,29 @@
 <?php
+
+/**
+ * EXHIBIT A. Common Public Attribution License Version 1.0
+ * The contents of this file are subject to the Common Public Attribution License Version 1.0 (the “License”);
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.dream-cms.kg/en/license. The License is based on the Mozilla Public License Version 1.1
+ * but Sections 14 and 15 have been added to cover use of software over a computer network and provide for
+ * limited attribution for the Original Developer. In addition, Exhibit A has been modified to be consistent
+ * with Exhibit B. Software distributed under the License is distributed on an “AS IS” basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the specific language
+ * governing rights and limitations under the License. The Original Code is Dream CMS software.
+ * The Initial Developer of the Original Code is Dream CMS (http://www.dream-cms.kg).
+ * All portions of the code written by Dream CMS are Copyright (c) 2014. All Rights Reserved.
+ * EXHIBIT B. Attribution Information
+ * Attribution Copyright Notice: Copyright 2014 Dream CMS. All rights reserved.
+ * Attribution Phrase (not exceeding 10 words): Powered by Dream CMS software
+ * Attribution URL: http://www.dream-cms.kg/
+ * Graphic Image as provided in the Covered Code.
+ * Display of Attribution Information is required in Larger Works which are defined in the CPAL as a work
+ * which combines Covered Code or portions thereof with code not governed by the terms of the CPAL.
+ */
 namespace Install;
 
 use Zend\ModuleManager\ModuleManagerInterface;
-use Zend\ModuleManager\ModuleEvent as ModuleEvent;
+use Zend\Mvc\MvcEvent as ModuleEvent;
 use Zend\Validator\AbstractValidator;
 use Zend\Mvc\MvcEvent;
 use Locale;
@@ -10,31 +31,36 @@ use Locale;
 class Module
 {
     /**
-     * Service managerzend
-     * @var object
+     * Service locator
+     *
+     * @var \Zend\ServiceManager\ServiceManager
      */
     protected $serviceLocator;
 
     /**
      * List of registered localizations
+     *
      * @var array
      */
     protected static $localizations;
 
     /**
      * Default localization
+     *
      * @var array
      */
     protected static $defaultLocalization;
 
     /**
      * Current localization
+     *
      * @var array
      */
     protected static $currentLocalization;
 
     /**
-     * Intl loaded
+     * Is Intl loaded
+     *
      * @var boolean
      */
     protected $intlLoaded;
@@ -42,7 +68,8 @@ class Module
     /**
      * Init
      *
-     * @param object $moduleManager
+     * @param \Zend\ModuleManager\ModuleManagerInterface $moduleManager
+     * @return void
      */
     public function init(ModuleManagerInterface $moduleManager)
     {
@@ -50,16 +77,17 @@ class Module
         $this->serviceLocator = $moduleManager->getEvent()->getParam('ServiceManager');
 
         $moduleManager->getEventManager()->
-            attach(ModuleEvent::EVENT_LOAD_MODULES_POST, [$this, 'initApplication']);
+                attach(ModuleEvent::EVENT_LOAD_MODULES_POST, [$this, 'initApplication']);
     }
 
     /**
      * Bootstrap
+     *
+     * @param \Zend\Mvc\MvcEvent $e
+     * @return void
      */
     public function onBootstrap(MvcEvent $e)
     {
-        $request = $this->serviceLocator->get('Request');
-
         // init user localization
         $e->getApplication()->getEventManager()->attach(MvcEvent::EVENT_DISPATCH, [
             $this, 'initUserLocalization'
@@ -69,7 +97,8 @@ class Module
     /**
      * Init application
      * 
-     * @param object $e
+     * @param \Zend\Mvc\MvcEvent $e
+     * @return void
      */
     public function initApplication(ModuleEvent $e)
     {
@@ -119,7 +148,8 @@ class Module
     /**
      * Init user localization
      *
-     * @param object $e MvcEvent
+     * @param \Zend\Mvc\MvcEvent $e
+     * @return void
      */
     public function initUserLocalization(MvcEvent $e)
     {
@@ -133,11 +163,14 @@ class Module
             if (!$matches->getParam('language')) {
                 // set default language
                 $router->setDefaultParam('language', self::$defaultLocalization['language']);
+
                 return;
             }
 
             // show a 404 page
-            return $matches->setParam('action', 'not-found');
+            $matches->setParam('action', 'not-found');
+
+            return;
         }
 
         // init an user localization
@@ -158,6 +191,8 @@ class Module
 
     /**
      * Init php settings
+     *
+     * @return void
      */
     protected function initPhpSettings()
     {
@@ -179,13 +214,13 @@ class Module
     {
         return [
             'Zend\Loader\ClassMapAutoloader' => [
-                __DIR__ . '/autoload_classmap.php',
+                __DIR__ . '/autoload_classmap.php'
             ],
             'Zend\Loader\StandardAutoloader' => [
                 'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ],
-            ],
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__
+                ]
+            ]
         ];
     }
 
@@ -207,6 +242,8 @@ class Module
 
     /**
      * Init view helpers
+     *
+     * @return array
      */
     public function getViewHelperConfig()
     {
@@ -225,7 +262,7 @@ class Module
     /**
      * Return path to config file
      *
-     * @return boolean
+     * @return array
      */
     public function getConfig()
     {
--- module/Install/src/Install/Model/InstallBase.php
+++ module/Install/src/Install/Model/InstallBase.php
@@ -1,8 +1,27 @@
 <?php
 
+/**
+ * EXHIBIT A. Common Public Attribution License Version 1.0
+ * The contents of this file are subject to the Common Public Attribution License Version 1.0 (the “License”);
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.dream-cms.kg/en/license. The License is based on the Mozilla Public License Version 1.1
+ * but Sections 14 and 15 have been added to cover use of software over a computer network and provide for
+ * limited attribution for the Original Developer. In addition, Exhibit A has been modified to be consistent
+ * with Exhibit B. Software distributed under the License is distributed on an “AS IS” basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the specific language
+ * governing rights and limitations under the License. The Original Code is Dream CMS software.
+ * The Initial Developer of the Original Code is Dream CMS (http://www.dream-cms.kg).
+ * All portions of the code written by Dream CMS are Copyright (c) 2014. All Rights Reserved.
+ * EXHIBIT B. Attribution Information
+ * Attribution Copyright Notice: Copyright 2014 Dream CMS. All rights reserved.
+ * Attribution Phrase (not exceeding 10 words): Powered by Dream CMS software
+ * Attribution URL: http://www.dream-cms.kg/
+ * Graphic Image as provided in the Covered Code.
+ * Display of Attribution Information is required in Larger Works which are defined in the CPAL as a work
+ * which combines Covered Code or portions thereof with code not governed by the terms of the CPAL.
+ */
 namespace Install\Model;
 
-use Zend\Http\Header\ContentDisposition;
 use Zend\Math\Rand;
 use Zend\Db\Adapter\Adapter as DbAdapter;
 use Transliterator;
@@ -12,7 +31,18 @@ use ReflectionExtension;
 class InstallBase
 {
     /**
+     * Site salt length
+     */
+    CONST SITE_SALT_LENGTH = 15;
+
+    /**
+     * Site salt chars
+     */
+    CONST SITE_SALT_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789,._-=+:*';
+
+    /**
      * System modules
+     *
      * @var array
      */
     protected $systemModules = [
@@ -28,6 +58,7 @@ class InstallBase
 
     /**
      * Writable resources
+     *
      * @var array
      */
     protected $writableResources = [
@@ -50,6 +81,7 @@ class InstallBase
 
     /**
      * Php basic extensions
+     *
      * @var array
      */
     protected $phpBasicExtensions = [
@@ -64,6 +96,7 @@ class InstallBase
 
     /**
      * Php settings
+     *
      * @var array
      */
     protected $phpSettings = [
@@ -73,6 +106,7 @@ class InstallBase
 
     /**
      * Enabled php functions
+     *
      * @var array
      */
     protected $phpEnabledFunctions = [
@@ -80,44 +114,39 @@ class InstallBase
     ];
 
     /**
-     * Php version 
+     * Php version
+     *
      * @var string
      */
     protected $phpVersion = '5.5';
 
     /**
      * Intl version
+     *
      * @var string
      */
     protected $intlVersion = '1.1.0';
 
     /**
-     * Intl icu version
+     * Intl ICU version
+     *
      * @var string
      */
     protected $intlIcuVersion = '50.1';
 
     /**
      * Install sql file
+     *
      * @var string
      */
     protected $installSqlFile = 'install.sql';
 
     /**
      * Site salt
+     *
      * @var string
      */
     protected $siteSalt;
-
-    /**
-     * Site salt length
-     */
-    CONST SITE_SALT_LENGTH = 15;
-
-    /**
-     * Site salt chars
-     */
-    CONST SITE_SALT_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789,._-=+:*';
 
     /**
      * Get site salt
@@ -141,7 +170,7 @@ class InstallBase
     public function getCronJobs()
     {
         return [
-            0 => [
+            [
                 'time'   => '*/5 * * * *',
                 'action' => APPLICATION_PUBLIC . '/index.php application send messages &> /dev/null'
             ]
@@ -348,7 +377,7 @@ CONTENT;
     /**
      * Execute sql file
      *
-     * @param object $adapter
+     * @param \Zend\Db\Adapter\Adapter $adapter
      * @param array $replace
      *      string from
      *      string to
@@ -365,7 +394,6 @@ CONTENT;
 
         $query = null;
         $delimiter = ';';
-        $result = [];
 
         // collect all queries
         while(!feof($handler)) {
@@ -382,7 +410,7 @@ CONTENT;
 
             $query .= ' ' . $str;
 
-            // check for multiline query
+            // check for multi line query
             if(substr($str, -strlen($delimiter)) != $delimiter) {
                 continue;
             }
@@ -436,7 +464,7 @@ CONTENT;
     }
 
     /**
-     * Get disabled php funcitons
+     * Get list of disabled php functions
      *
      * @return array
      */
@@ -460,7 +488,7 @@ CONTENT;
     }
 
     /**
-     * Get not installed php extensions
+     * Get list of not installed php extensions
      *
      * @return array
      */
@@ -524,6 +552,7 @@ CONTENT;
         $output = strip_tags(ob_get_clean());
 
         preg_match('/^ICU version +(?:=> )?(.*)$/m', $output, $matches);
+
         return $matches[1];
     }
 
@@ -570,18 +599,18 @@ CONTENT;
      * 
      * @param string $title
      * @param integer $maxChars
-     * @param string $spaceDevider
+     * @param string $spaceDivider
      * @param integer $objectId
      * @param string $pattern
      * @return string
      */
-    protected function slugify($title, $maxChars = 100, $spaceDevider = '-', $objectId = 0, $pattern = '0-9a-z\s')
+    protected function slugify($title, $maxChars = 100, $spaceDivider = '-', $objectId = 0, $pattern = '0-9a-z\s')
     {
         $transliterator = Transliterator::create('Any-Latin; Latin-ASCII; Lower();');
         $title = preg_replace('/[^' . $pattern. ']/i', '', $transliterator->transliterate($title));
-        $title = str_replace(' ', $spaceDevider, $title);
+        $title = str_replace(' ', $spaceDivider, $title);
 
-        $slug = $objectId ? $objectId . $spaceDevider . $title : $title;
+        $slug = $objectId ? $objectId . $spaceDivider . $title : $title;
 
         return strlen($slug) > $maxChars ? substr($slug, 0, $maxChars) : $slug;
     }
