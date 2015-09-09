5e204cf60df7be8f81acac57b636b48760acf395
esase <alexermashev@gmail.com>
2015-09-09 16:25:15 +0600

    Add a license info in User module

--- module/User/src/User/Form/User.php
+++ module/User/src/User/Form/User.php
@@ -1,4 +1,25 @@
 <?php
+
+/**
+ * EXHIBIT A. Common Public Attribution License Version 1.0
+ * The contents of this file are subject to the Common Public Attribution License Version 1.0 (the “License”);
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.dream-cms.kg/en/license. The License is based on the Mozilla Public License Version 1.1
+ * but Sections 14 and 15 have been added to cover use of software over a computer network and provide for
+ * limited attribution for the Original Developer. In addition, Exhibit A has been modified to be consistent
+ * with Exhibit B. Software distributed under the License is distributed on an “AS IS” basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the specific language
+ * governing rights and limitations under the License. The Original Code is Dream CMS software.
+ * The Initial Developer of the Original Code is Dream CMS (http://www.dream-cms.kg).
+ * All portions of the code written by Dream CMS are Copyright (c) 2014. All Rights Reserved.
+ * EXHIBIT B. Attribution Information
+ * Attribution Copyright Notice: Copyright 2014 Dream CMS. All rights reserved.
+ * Attribution Phrase (not exceeding 10 words): Powered by Dream CMS software
+ * Attribution URL: http://www.dream-cms.kg/
+ * Graphic Image as provided in the Covered Code.
+ * Display of Attribution Information is required in Larger Works which are defined in the CPAL as a work
+ * which combines Covered Code or portions thereof with code not governed by the terms of the CPAL.
+ */
 namespace User\Form;
 
 use Application\Service\ApplicationSetting as SettingService;
@@ -7,7 +28,7 @@ use Application\Form\ApplicationAbstractCustomForm;
 use Application\Form\ApplicationCustomFormBuilder;
 use User\Model\UserBase as UserBaseModel;
 
-class User extends ApplicationAbstractCustomForm 
+class User extends ApplicationAbstractCustomForm
 {
     /**
      * Slug max string length
@@ -46,48 +67,56 @@ class User extends ApplicationAbstractCustomForm
 
     /**
      * Form name
+     *
      * @var string
      */
     protected $formName = 'user';
 
     /**
      * List of ignored elements
+     *
      * @var array
      */
     protected $ignoredElements = ['confirm_password', 'captcha', 'avatar'];
 
     /**
      * Model instance
-     * @var object  
+     *
+     * @var \User\Model\UserBase
      */
     protected $model;
 
     /**
      * Time zones
+     *
      * @var array
      */
     protected $timeZones;
 
     /**
      * User id
+     *
      * @var integer
      */
     protected $userId;
 
     /**
      * User avatar
+     *
      * @var string
      */
     protected $avatar;
 
     /**
      * Captcha enabled flag
+     *
      * @var boolean
      */
     protected $isCaptchaEnabled = false;
 
     /**
      * Form elements
+     *
      * @var array
      */
     protected $formElements = [
@@ -205,7 +234,7 @@ class User extends ApplicationAbstractCustomForm
     /**
      * Get form instance
      *
-     * @return object
+     * @return \Application\Form\ApplicationCustomFormBuilder
      */
     public function getForm()
     {
@@ -305,12 +334,13 @@ class User extends ApplicationAbstractCustomForm
     /**
      * Set a model
      *
-     * @param object $model
-     * @return object fluent interface
+     * @param \User\Model\UserBase $model
+     * @return \User\Form\User
      */
     public function setModel(UserBaseModel $model)
     {
         $this->model = $model;
+
         return $this;
     }
 
@@ -318,11 +348,12 @@ class User extends ApplicationAbstractCustomForm
      * Set time zones
      *
      * @param array $timeZones
-     * @return object fluent interface
+     * @return \User\Form\User
      */
     public function setTimeZones(array $timeZones)
     {
         $this->timeZones = $timeZones;
+
         return $this;
     }
 
@@ -330,11 +361,12 @@ class User extends ApplicationAbstractCustomForm
      * Set a user id
      *
      * @param integer $userId
-     * @return object fluent interface
+     * @return \User\Form\User
      */
     public function setUserId($userId)
     {
         $this->userId = $userId;
+
         return $this;
     }
 
@@ -342,11 +374,12 @@ class User extends ApplicationAbstractCustomForm
      * Set a user avatar
      *
      * @param string $avatar
-     * @return object fluent interface
+     * @return \User\Form\User
      */
     public function setUserAvatar($avatar)
     {
         $this->avatar = $avatar;
+
         return $this;
     }
 
@@ -354,11 +387,12 @@ class User extends ApplicationAbstractCustomForm
      * Show captcha
      *
      * @param boolean $state
-     * @return object fluent interface
+     * @return \User\Form\User
      */
     public function showCaptcha($state)
     {
         $this->isCaptchaEnabled = $state;
+
         return $this;
     }
 
