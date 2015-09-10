c7ab531bb42743f189a06a2aec561e24cf474b6f
esase <alexermashev@gmail.com>
2015-09-10 10:38:15 +0600

    Add a license info in User module part #2

--- module/User/src/User/View/Widget/UserRegisterWidget.php
+++ module/User/src/User/View/Widget/UserRegisterWidget.php
@@ -1,4 +1,25 @@
 <?php
+
+/**
+ * EXHIBIT A. Common Public Attribution License Version 1.0
+ * The contents of this file are subject to the Common Public Attribution License Version 1.0 (the “License”);
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.dream-cms.kg/en/license. The License is based on the Mozilla Public License Version 1.1
+ * but Sections 14 and 15 have been added to cover use of software over a computer network and provide for
+ * limited attribution for the Original Developer. In addition, Exhibit A has been modified to be consistent
+ * with Exhibit B. Software distributed under the License is distributed on an “AS IS” basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the specific language
+ * governing rights and limitations under the License. The Original Code is Dream CMS software.
+ * The Initial Developer of the Original Code is Dream CMS (http://www.dream-cms.kg).
+ * All portions of the code written by Dream CMS are Copyright (c) 2014. All Rights Reserved.
+ * EXHIBIT B. Attribution Information
+ * Attribution Copyright Notice: Copyright 2014 Dream CMS. All rights reserved.
+ * Attribution Phrase (not exceeding 10 words): Powered by Dream CMS software
+ * Attribution URL: http://www.dream-cms.kg/
+ * Graphic Image as provided in the Covered Code.
+ * Display of Attribution Information is required in Larger Works which are defined in the CPAL as a work
+ * which combines Covered Code or portions thereof with code not governed by the terms of the CPAL.
+ */
 namespace User\View\Widget;
 
 use User\Service\UserIdentity as UserIdentityService;
@@ -15,9 +36,7 @@ class UserRegisterWidget extends UserAbstractWidget
      */
     public function getContent() 
     {
-        if (UserIdentityService::isGuest() 
-                && (int) $this->getSetting('user_allow_register')) {
-
+        if (UserIdentityService::isGuest() && (int) $this->getSetting('user_allow_register')) {
             // get an user form
             $userForm = $this->getServiceLocator()
                 ->get('Application\Form\FormManager')
@@ -44,8 +63,8 @@ class UserRegisterWidget extends UserAbstractWidget
                     // add a new user with a particular status
                     $status = (int) $this->getSetting('user_auto_confirm') ? true : false;
 
-                    $userInfo = $this->getModel()->addUser($userForm->getForm()->getData(), 
-                        LocalizationService::getCurrentLocalization()['language'], $status, $this->getRequest()->getFiles()->avatar, true);
+                    $userInfo = $this->getModel()->addUser($userForm->getForm()->getData(),
+                            LocalizationService::getCurrentLocalization()['language'], $status, $this->getRequest()->getFiles()->avatar, true);
 
                     // the user has been added
                     if (is_array($userInfo)) {
