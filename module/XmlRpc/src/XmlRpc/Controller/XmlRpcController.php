f466a3a019b4cfe89ed7d4382912547e8d815ee1
esase <alexermashev@gmail.com>
2015-09-10 11:20:43 +0600

    Add a license info in XmlRpc module

--- module/XmlRpc/src/XmlRpc/Controller/XmlRpcController.php
+++ module/XmlRpc/src/XmlRpc/Controller/XmlRpcController.php
@@ -1,4 +1,25 @@
 <?php
+
+/**
+ * EXHIBIT A. Common Public Attribution License Version 1.0
+ * The contents of this file are subject to the Common Public Attribution License Version 1.0 (the “License”);
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.dream-cms.kg/en/license. The License is based on the Mozilla Public License Version 1.1
+ * but Sections 14 and 15 have been added to cover use of software over a computer network and provide for
+ * limited attribution for the Original Developer. In addition, Exhibit A has been modified to be consistent
+ * with Exhibit B. Software distributed under the License is distributed on an “AS IS” basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the specific language
+ * governing rights and limitations under the License. The Original Code is Dream CMS software.
+ * The Initial Developer of the Original Code is Dream CMS (http://www.dream-cms.kg).
+ * All portions of the code written by Dream CMS are Copyright (c) 2014. All Rights Reserved.
+ * EXHIBIT B. Attribution Information
+ * Attribution Copyright Notice: Copyright 2014 Dream CMS. All rights reserved.
+ * Attribution Phrase (not exceeding 10 words): Powered by Dream CMS software
+ * Attribution URL: http://www.dream-cms.kg/
+ * Graphic Image as provided in the Covered Code.
+ * Display of Attribution Information is required in Larger Works which are defined in the CPAL as a work
+ * which combines Covered Code or portions thereof with code not governed by the terms of the CPAL.
+ */
 namespace XmlRpc\Controller;
 
 use Application\Controller\ApplicationAbstractBaseController;
@@ -11,12 +32,15 @@ class XmlRpcController extends ApplicationAbstractBaseController
 {
     /**
      * Model instance
-     * @var object  
+     *
+     * @var \XmlRpc\Model\XmlRpc
      */
     protected $model;
 
     /**
      * Get model
+     *
+     * @return \XmlRpc\Model\XmlRpc
      */
     protected function getModel()
     {
@@ -34,7 +58,7 @@ class XmlRpcController extends ApplicationAbstractBaseController
      */
     public function indexAction()
     {
-        // get user info by api key
+        // get user info by the api key
         if (null != ($apiKey = $this->getRequest()->getQuery()->api_key)) {
             if (null != ($userInfo =
                     UserIdentityService::getUserInfo($apiKey, UserModelBase::USER_INFO_BY_API_KEY))) {
--- module/XmlRpc/src/XmlRpc/Handler/XmlRpcAbstractHandler.php
+++ module/XmlRpc/src/XmlRpc/Handler/XmlRpcAbstractHandler.php
@@ -1,4 +1,25 @@
 <?php
+
+/**
+ * EXHIBIT A. Common Public Attribution License Version 1.0
+ * The contents of this file are subject to the Common Public Attribution License Version 1.0 (the “License”);
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.dream-cms.kg/en/license. The License is based on the Mozilla Public License Version 1.1
+ * but Sections 14 and 15 have been added to cover use of software over a computer network and provide for
+ * limited attribution for the Original Developer. In addition, Exhibit A has been modified to be consistent
+ * with Exhibit B. Software distributed under the License is distributed on an “AS IS” basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the specific language
+ * governing rights and limitations under the License. The Original Code is Dream CMS software.
+ * The Initial Developer of the Original Code is Dream CMS (http://www.dream-cms.kg).
+ * All portions of the code written by Dream CMS are Copyright (c) 2014. All Rights Reserved.
+ * EXHIBIT B. Attribution Information
+ * Attribution Copyright Notice: Copyright 2014 Dream CMS. All rights reserved.
+ * Attribution Phrase (not exceeding 10 words): Powered by Dream CMS software
+ * Attribution URL: http://www.dream-cms.kg/
+ * Graphic Image as provided in the Covered Code.
+ * Display of Attribution Information is required in Larger Works which are defined in the CPAL as a work
+ * which combines Covered Code or portions thereof with code not governed by the terms of the CPAL.
+ */
 namespace XmlRpc\Handler;
 
 use User\Service\UserIdentity as UserIdentityService;
@@ -6,18 +27,6 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 
 abstract class XmlRpcAbstractHandler
 {
-    /**
-     * Service locator
-     * @var object
-     */
-    protected $serviceLocator;
-
-    /**
-     * User identity
-     * @var array
-     */
-    protected $userIdentity;
-
     /**
      * Successfully response flag
      */
@@ -36,12 +45,26 @@ abstract class XmlRpcAbstractHandler
     /**
      * Request is broken
      */
-    const REQUEST_BROKEN = 'Error occured';
+    const REQUEST_BROKEN = 'Error occurred';
+
+    /**
+     * Service locator
+     *
+     * @var \Zend\ServiceManager\ServiceLocatorInterface
+     */
+    protected $serviceLocator;
+
+    /**
+     * User identity
+     *
+     * @var array
+     */
+    protected $userIdentity;
 
     /**
      * Class constructor
      *
-     * @param object $serviceLocator
+     * @param \Zend\ServiceManager\ServiceLocatorInterface $serviceLocator
      */
     public function __construct(ServiceLocatorInterface $serviceLocator)
     {
@@ -65,7 +88,6 @@ abstract class XmlRpcAbstractHandler
 
         asort($args);
 
-        return $requestSignature ==
-                md5(implode(':', array_merge($args, [$this->userIdentity['api_secret']])));
+        return $requestSignature == md5(implode(':', array_merge($args, [$this->userIdentity['api_secret']])));
     }
 }
